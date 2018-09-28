1d70d83a02da4dfa76c8908c341f8f8725fb1d1d
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-28 17:43:45 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,12 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  if season === "season 15" 
+    return "Emily"
+    elsif season === "season 16"
+    return "Courtney" 
+  else season === "season 10" 
+    return "Tessa"
+  end 
 end
 
 def get_contestant_name(data, occupation)
